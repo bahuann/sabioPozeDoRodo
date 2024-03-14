69afacf3d0a7bb3a8899000c6643a47b4b1041df
bahuann <140553582+bahuann@users.noreply.github.com>
2024-03-13 23:43:40 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Configurações do banco de dados - substitua pelos seus valores
+DB_HOST = "datasources.postgres.database.azure.com"
+DB_NAME = "db_product_manager_prd"
+DB_USER = "a55dba"
+DB_PASS = "Bzjlep2010#lima"
 
-@app.route('/about')
-def about():
-    return 'About'
+def get_db_connection():
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    return conn
+
+@app.route('/total_orders_by_status')
+def total_orders_by_status():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT status, COUNT(*) AS number_of_orders FROM orders GROUP BY status;")
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(results)
+
+@app.route('/average_time_by_status')
+def average_time_by_status():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT o.status, AVG(age(h.created_at, o.created_at)) AS average_duration FROM orders o INNER JOIN orders_histories h ON o.id = h.order_id GROUP BY o.status;")
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
